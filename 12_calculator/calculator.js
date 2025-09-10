064beade052fcad319e32aa93484bff6d12d24e6
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-09-10 17:44:11 +0300

    Complete the calculator assignment

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,56 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+	  return array.reduce((acc, currNumber) => acc + currNumber);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+	return array.reduce((acc, currNumber) => acc * currNumber);
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    let numberArray = [];
+    for (let i = 0; i < b; i++) {
+      numberArray.push(a)
+    }
+
+	if (b > 0) {
+    return numberArray.reduce((acc, currNumber) => acc * currNumber);
+
+  } else if (b < 0) {
+    let divisor = numberArray.reduce((acc, currNumber) => acc * currNumber);
+    return 1 / divisor;
+
+  } else if (b === 0) {
+    return 1;
+  }
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if (a === 0) {
+    return 1
+  } else if (a > 0) {
+      let numberArray = [];
+      for (let i = a; i > 0; i--) {
+        numberArray.push(a);
+        a--;
+      }
+      return numberArray.reduce((acc, currNumber) => acc * currNumber);
+  }
 	
 };
 
