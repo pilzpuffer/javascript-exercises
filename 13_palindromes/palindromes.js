fb40af55d0ada7ecde9a0b097dc8695ca2f54517
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-09-11 05:38:51 +0300

    Complete the palindromes task

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphabet = [...'abcdefghijklmnopqrstuvwxyz1234567890'];
+    let toCharacters = string.toLowerCase().split(""); //divide string into characters
+    let isLetter = function (value) {
+        if (alphabet.includes(value.toLowerCase())) {
+            return value;
+        }
+    }
 
+    let isPalindrome = toCharacters.filter(isLetter);
+    let isPalindromeReversed = isPalindrome.toReversed();
+
+    console.log(isPalindrome);
+    console.log(isPalindromeReversed);
+    
+    let getAnswer = function (array1, array2) {
+        return array1.every((element, index) => element === array2[index]);
+    }
+
+    return getAnswer(isPalindrome, isPalindromeReversed);
 };
 
 // Do not edit below this line
