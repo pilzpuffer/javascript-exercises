0baea9df56b6bb18f9e9ed9750fe8080a67fbea8
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-09-12 23:44:53 +0300

    Complete the fibonacci sequence task

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(seriesElement) {
+    //1, 1, 2, 3, 5, 8
+
+    let fibArray = []
+    if (typeof(seriesElement) !== "number") {
+        seriesElement = Number(seriesElement);
+    }
+
+    if (seriesElement < 0) {
+        return "OOPS";
+    }
+
+    if (seriesElement === 0) {
+        return 0;
+    }  else {
+        for (let i = 0; i < seriesElement; i++) {
+            if (i < 2) {
+                fibArray.push(1);
+            } else {
+                fibArray.push(fibArray[i-2] + fibArray[i-1]);
+                console.log(fibArray);
+            }
+        }
+
+        let fibValue =  fibArray.slice(-1).pop();
+        console.log(typeof(fibValue));
+        return fibValue;
+    }
 
 };
 
